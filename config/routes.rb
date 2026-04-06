32f4ed974f3576aa2b0daceb0a5c7a91beac6fe2
MaruYuki0927 <211195289+MaruYuki0927@users.noreply.github.com>
2026-04-07 00:05:07 +0900

    ルートを直した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/new'
+  root "homes#top"
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
