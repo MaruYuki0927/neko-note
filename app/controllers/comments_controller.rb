55e9145dececdf6589d4beca36921198fa7438b9
MaruYuki0927 <211195289+MaruYuki0927@users.noreply.github.com>
2026-04-16 19:08:50 +0900

    コメント機能

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,33 @@
+class CommentsController < ApplicationController
+
+    before_action :authenticate_user!
+  
+    def create
+      @post = Post.find(params[:post_id])
+      @comment = current_user.comments.build(comment_params)
+      @comment.post_id = @post.id
+  
+      if @comment.save
+        redirect_to post_path(@post)
+      else
+        redirect_to post_path(@post)
+      end
+    end
+
+    def destroy
+      @comment = Comment.find(params[:id])
+    
+      if @comment.user == current_user
+        @comment.destroy
+      end
+    
+      redirect_to post_path(@comment.post)
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   #belongs_to :cat
   has_one_attached :image
   belongs_to :user
+  has_many :comments, dependent: :destroy
 end
